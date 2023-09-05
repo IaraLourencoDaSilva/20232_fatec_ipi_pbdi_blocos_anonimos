64ebc90a593d7ee6394cf0f343fde77bf95ed8dd
Iara <iara.silva14@fatec.sp.gov.br>
2023-09-05 10:15:20 -0300

    ap08(blocos-anonimos): codigo 2.5.1

--- script.sql
+++ script.sql
@@ -1,18 +1,47 @@
-DO
+DO 
 $$
 DECLARE 
-	codigo INTEGER := 1;
-	nome_completo VARCHAR(200):= 'João Santos';
-	-- 11 digitos no total, 2 para valores decimais 
-	salario NUMERIC(11,2) := 20.5;
+	n1 INT := 5;
+	n2 INT := 2;
+	n3 NUMERIC (5, 2) := 5;
+	n4 INT :=  -5;
 BEGIN
--- 	Fazer um raise notice que exibe o seguinte 
--- Meu código é tal, me chamo tal e meu salario é tal 
-	RAISE NOTICE 'Meu código é %, Me chamo %, meu salario é %', 02, 'Iara', 1.300;
-	
-	
+	-- divisão inteira 
+	RAISE NOTICE '% / % = %', n1, n2, n1/n2;
+	--divisão real 
+	RAISE NOTICE '% / % = %', n3, n2, n3 / n2;
+	--resto da divisão 
+	RAISE NOTICE '% %% % = %', n1, n2, n1 % n2;
+	-- Exponenciação
+	RAISE NOTICE '% ^ % = %', n1, n2, n1 ^ n2;
+	-- Raiz quadrada
+	RAISE NOTICE '|/ % = %', n1, |/n1;
+	--Raiz cubica
+	RAISE NOTICE '||?% = %', n1, ||/n1;
+	-- Valor absoluto - ignorar o sinal 
+	RAISE NOTICE '@% = % e @% = %', n1, @n1, n4, @n4;
 END;
 $$
+
+
+
+
+
+-- DO
+-- $$
+-- DECLARE 
+-- 	codigo INTEGER := 1;
+-- 	nome_completo VARCHAR(200):= 'João Santos';
+-- 	-- 11 digitos no total, 2 para valores decimais 
+-- 	salario NUMERIC(11,2) := 20.5;
+-- BEGIN
+-- -- 	Fazer um raise notice que exibe o seguinte 
+-- -- Meu código é tal, me chamo tal e meu salario é tal 
+-- 	RAISE NOTICE 'Meu código é %, Me chamo %, meu salario é %', 02, 'Iara', 1.300;
+	
+	
+-- END;
+-- $$
 
 
 
